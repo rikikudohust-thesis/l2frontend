6ec3102b13940faf1cd5f9319e10100fec804f59
rikikudohust <trunghieubh0102@gmail.com>
2023-07-14 12:43:36 +0700

    dashboard: custom

---
+++ src/components/modal/CreateWithdrawModel.js
@@ -0,0 +1,266 @@
+import {
+    Box,
+    Button,
+    Input,
+    InputAdornment,
+    IconButton,
+    MenuList,
+    MenuItem,
+    Popover,
+  } from "@mui/material";
+  import CancelIcon from "@mui/icons-material/Cancel";
+  import { useState } from "react";
+  import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+  
+  const mockData = [
+    {
+      nameTag: "Account #1",
+      owner: "0x242ed78bf0fe7672ff01ae6de558e45b3749f197",
+      address: "0x2a26bca625b223971909dd88fc93faeb050dc5b3",
+      balance: {
+        USDT: 500000,
+        USDC: 200000,
+        ETH: 50,
+        WBTC: 20,
+      },
+    },
+    {
+      nameTag: "Account #2",
+      owner: "0xb3eC5Db932736D0203004FD7208b9b007d166B35",
+      address: "0x637dD76d7a4a80Ad9f9f09830B50DC384454bF27",
+      balance: {
+        USDT: 200000,
+        USDC: 500000,
+        ETH: 20,
+        WBTC: 10,
+      },
+    },
+  ];
+  const tokens = [
+    {
+      name: "ETH",
+      address: "0x00000000",
+      decimals: 18,
+    },
+    {
+      name: "USDC",
+      address: "0x00000001",
+      decimals: 6,
+    },
+    {
+      name: "USDT",
+      address: "0x00000002",
+      decimals: 6,
+    },
+    {
+      name: "WBTC",
+      address: "0x00000003",
+      decimals: 8,
+    },
+  ];
+  
+  function CreateWithdrawModal({ handleClose }) {
+    const [senderAnchorEl, setSenderAnchorEl] = useState(null);
+    const [tokenAnchorEl, setTokenAnchorEl] = useState(null);
+    const [sender, setSender] = useState(mockData[0].address);
+    const [token, setToken] = useState(tokens[0].name);
+    const [receiver, setReceiver] = useState("");
+    const [amount, setAmount] = useState(0);
+  
+    function getBalance() {
+      return mockData.find((item) => item.address === sender).balance[token];
+    }
+  
+    return (
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          backgroundColor: "#111827",
+          width: "40%",
+          height: "40%",
+          borderRadius: "25px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            paddingTop: "10px",
+            paddingRight: "10px",
+            width: "100%",
+          }}
+        >
+          <IconButton onClick={handleClose}>
+            <CancelIcon sx={{ color: "#FFF" }} />
+          </IconButton>
+        </Box>
+        <Box
+          sx={{ display: "flex", justifyContent: "center", paddingTop: "20px" }}
+        >
+          New Withdraw
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            bgcolor: "#161E2D",
+            width: "80%",
+            borderRadius: "15px",
+            marginTop: "30px",
+            padding: "20px",
+          }}
+        >
+          <Box sx={{ width: "30%", display: "flex", flexDirection: "column" }}>
+            <Box sx={{ fontWeight: "600" }}>Token</Box>
+            <Box pt={2} width="100%">
+              <Button
+                sx={{
+                  borderRadius: "15px",
+                  fontFamily: "inherit",
+                  color: "inherit",
+                  border: "2px solid #5C80BC",
+                  width: "70%",
+                  height: "50px",
+                }}
+                onClick={(e) => setTokenAnchorEl(e.currentTarget)}
+              >
+                <Box width="80%">{token}</Box>
+                <Box width="20%" display="flex" alignItems="center">
+                  <KeyboardArrowRightIcon />
+                </Box>
+              </Button>
+              <Popover
+                anchorEl={tokenAnchorEl}
+                sx={{
+                  ".MuiPopover-paper": {
+                    borderRadius: "15px",
+                  },
+                }}
+                open={Boolean(tokenAnchorEl)}
+                onClose={() => setTokenAnchorEl(null)}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "center",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "center",
+                }}
+              >
+                <Box
+                  sx={{
+                    border: "2px solid #5C80BC",
+                    bgcolor: "#192238",
+                    borderRadius: "15px",
+                    color: "#FFF",
+                    paddingX: "20px",
+                  }}
+                >
+                  <MenuList>
+                    {tokens.map(
+                      (item) =>
+                        item.name !== token && (
+                          <MenuItem
+                            sx={{ fontFamily: "Lexend Exa" }}
+                            onClick={() => setToken(item.name)}
+                          >
+                            {item.name}
+                          </MenuItem>
+                        )
+                    )}
+                  </MenuList>
+                </Box>
+              </Popover>
+            </Box>
+          </Box>
+          <Box sx={{ width: "70%", display: "flex", flexDirection: "column" }}>
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              sx={{ fontWeight: "600" }}
+            >
+              <Box>Amount</Box>
+              <Box>
+                Your balance : {getBalance()} {token}
+              </Box>
+            </Box>
+            <Box pt={2}>
+              <Input
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                type="number"
+                disableUnderline
+                color="#FFF"
+                sx={{
+                  paddingLeft: "10px",
+                  height: "50px",
+                  color: "#FFF",
+                  fontFamily: "inherit",
+                  fontSize: "18px",
+                  fontWeight: "500",
+                  border: "2px solid #5C80BC",
+                  borderRadius: "15px",
+                  width: "100%",
+                  "& input[type=number]": {
+                    "-moz-appearance": "textfield",
+                  },
+                  "& input[type=number]::-webkit-outer-spin-button": {
+                    "-webkit-appearance": "none",
+                    margin: 0,
+                  },
+                  "& input[type=number]::-webkit-inner-spin-button": {
+                    "-webkit-appearance": "none",
+                    margin: 0,
+                  },
+                }}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <Button
+                      sx={{
+                        width: "5%",
+                        fontSize: "10px",
+                        fontWeight: "600",
+                        textTransform: "none",
+                        color: "#FFF",
+                        border: "1px solid #5C80BC",
+                        borderRadius: "10px",
+                        fontFamily: "inherit",
+                        marginRight: "5px",
+                      }}
+                      onClick={() => setAmount(getBalance())}
+                    >
+                      MAX
+                    </Button>
+                  </InputAdornment>
+                }
+              />
+            </Box>
+          </Box>
+        </Box>
+        <Box width="90%" display="flex" justifyContent="flex-end" py={3}>
+          <Button
+            sx={{
+              width: "20%",
+              fontSize: "15px",
+              fontWeight: "600",
+              textTransform: "none",
+              color: "#FFF",
+              border: "1px solid #5C80BC",
+              borderRadius: "10px",
+              fontFamily: "inherit",
+            }}
+          >
+            Finish
+          </Button>
+        </Box>
+      </Box>
+    );
+  }
+  
+  export default CreateWithdrawModal;
+  
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,36 +2,33 @@ import { Box, Button, Modal } from "@mui/material";
 import Account from "../components/Accounts";
 import AddBoxIcon from "@mui/icons-material/AddBox";
 import CreateTransactionModal from "../components/modal/CreateTransactionModal";
+import CreateDepositModal from "../components/modal/CreateDepositModel";
+import CreateWithdrawModal from "../components/modal/CreateWithdrawModel";
 import { useState } from "react";
 
-
 function Dashboard() {
-  const [openModal, setOpenModal] = useState(false);
+  const [openModal, setOpenModal] = useState(0);
   const handleNewTx = () => {
-    setOpenModal(true);
+    setOpenModal(2);
+  };
+  const handleNewDeposit = () => {
+    setOpenModal(1);
+  };
+
+  const handleNewWithdraw = () => {
+    setOpenModal(3);
   };
 
   const mockData = [
     {
       nameTag: "Account #1",
       owner: "0x242ed78bf0fe7672ff01ae6de558e45b3749f197",
-      address: "0xcB714F263cBc742A79745faf2B2c47367460D26A",
+      bjj: "0xcB714F263cBc742A79745faf2B2c47367460D26A",
       balance: {
         USDT: 500000,
         USDC: 200000,
         ETH: 50,
         WBTC: 20,
-      },
-    },
-    {
-      nameTag: "Account #2",
-      owner: "0xb3eC5Db932736D0203004FD7208b9b007d166B35",
-      address: "0x990b82dc8ab6134f482d2cad3bba11c537cd7b45",
-      balance: {
-        USDT: 200000,
-        USDC: 500000,
-        ETH: 20,
-        WBTC: 10,
       },
     },
   ];
@@ -44,35 +41,67 @@ function Dashboard() {
           marginBottom: "15px",
         }}
       >
-        <Button
-          sx={{
-            bgcolor: "#416CB8",
-            padding: "10px",
-            borderRadius: "10px",
-            textTransform: "none",
-            fontFamily: "inherit",
-            color: "inherit",
-            "&:hover": {
+        <Box display="flex" justifyContent="space-between" width="31%">
+          <Button
+            sx={{
               bgcolor: "#416CB8",
-            },
-          }}
-          onClick={handleNewTx}
-        >
-          <AddBoxIcon sx={{ marginRight: "5px" }} />
-          New Transaction
-        </Button>
-        <Modal open={openModal} onClose={() => setOpenModal(false)}>
-          <CreateTransactionModal handleClose={() => setOpenModal(false)} />
+              padding: "10px",
+              borderRadius: "10px",
+              textTransform: "none",
+              fontFamily: "inherit",
+              color: "inherit",
+              "&:hover": {
+                bgcolor: "#416CB8",
+              },
+            }}
+            onClick={handleNewDeposit}
+          >
+            <AddBoxIcon sx={{ marginRight: "5px" }} />
+            New Deposit
+          </Button>
+          <Button
+            sx={{
+              bgcolor: "#416CB8",
+              padding: "10px",
+              borderRadius: "10px",
+              textTransform: "none",
+              fontFamily: "inherit",
+              color: "inherit",
+              "&:hover": {
+                bgcolor: "#416CB8",
+              },
+            }}
+            onClick={handleNewTx}
+          >
+            <AddBoxIcon sx={{ marginRight: "5px" }} />
+            New Withdraw
+          </Button>
+          <Button
+            sx={{
+              bgcolor: "#416CB8",
+              padding: "10px",
+              borderRadius: "10px",
+              textTransform: "none",
+              fontFamily: "inherit",
+              color: "inherit",
+              "&:hover": {
+                bgcolor: "#416CB8",
+              },
+            }}
+            onClick={handleNewWithdraw}
+          >
+            <AddBoxIcon sx={{ marginRight: "5px" }} />
+            New Transaction
+          </Button>
+        </Box>
+        <Modal open={openModal} onClose={() => setOpenModal(0)}>
+          {openModal === 1?<CreateDepositModal handleClose={() => setOpenModal(0)} />:openModal===2?<CreateWithdrawModal handleClose={() => setOpenModal(0)} />:<CreateTransactionModal handleClose={() => setOpenModal(0)} />}
+          {/* <CreateTransactionModal handleClose={() => setOpenModal(0)} /> */}
         </Modal>
       </Box>
 
       {mockData.map((item) => (
-        <Account
-          nameTag={item.nameTag}
-          owner={item.owner}
-          address={item.address}
-          balance={item.balance}
-        />
+        <Account nameTag={item.nameTag} owner={item.owner} address={item.bjj} balance={item.balance} />
       ))}
     </Box>
   );
